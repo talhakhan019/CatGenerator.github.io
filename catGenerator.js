4d33c30d4709bed0ec36eb519c613e0eedc0ac7b
talhakhan019 <56596055+talhakhan019@users.noreply.github.com>
2021-09-08 12:00:36 +0500

    Cat Generator Using JavaScript

---
+++ catGenerator.js
@@ -0,0 +1,5 @@
+function generate(){
+    var img= document.createElement('img');
+    img.src='https://cdn2.thecatapi.com/images/8kq.gif';
+    document.getElementById('generateCat').appendChild(img);
+}
